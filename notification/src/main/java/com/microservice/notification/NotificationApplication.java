5dd4b941d79e67789f4827795f4e8aba9e634d73
Vladyslav Holik <moyfolder@gmail.com>
2022-07-06 20:07:05 +0300

    Adding kubernetes config

--- notification/src/main/java/com/microservice/notification/NotificationApplication.java
+++ notification/src/main/java/com/microservice/notification/NotificationApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication(scanBasePackages = {
         "com.microservice.notification",
@@ -11,6 +13,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 })
 @EnableEurekaClient
 @EnableFeignClients(basePackages = "com.microservice.clients")
+@PropertySources({@PropertySource("classpath:clients-${spring.profiles.active}.properties")})
 public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
